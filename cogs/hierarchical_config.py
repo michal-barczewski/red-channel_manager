30cf1293fefdf49b5e9cd3885545f64cce2e5341
Michał Barczewski <michal.j.barczewski@gmail.com>
2016-09-11 19:30:27 +0200

    separate locations from values - Location class

--- cogs/hierarchical_config.py
+++ cogs/hierarchical_config.py
@@ -1,10 +1,19 @@
-from typing import Dict, List
+from typing import Dict, List, TypeVar, Union
+
+BaseValueType = TypeVar('BaseValueType', Union[str, int, float])
+ValueType = TypeVar('ValueType', Union[BaseValueType, List[BaseValueType]])
+
+
+class Location:
+    def __init__(self):
+        self.locations = dict()  # type: Dict[str, Location]
+        self.values = dict()  # type: Dict[str, ValueType]
 
 
 class HierarchicalConfig:
-    def __init__(self, defaults: Dict[str, object] = {}):
-        self.data = {}
-        self.defaults = defaults
+    def __init__(self, defaults: Dict[str, ValueType] = None):
+        self.data = Location()
+        self.defaults = {} if defaults is None else defaults # type: Dict[str, ValueType]
 
     def ensure_path(self, path: List[str]) -> Dict[str, Dict]:
         if len(path) == 0:
@@ -21,4 +30,14 @@ class HierarchicalConfig:
         location[name] = value
 
     def get_var(self, name, path):
-        pass
+        value = self.data.get(name, default=self.defaults.get(name))
+        location = self.data
+        for key in path:
+            # get value from current location, preserve existing one if it doesn't exist here
+            value = location.get(name, default=location)
+            if key in location:
+                location = location[key]
+            else:
+                break
+        return value
+
