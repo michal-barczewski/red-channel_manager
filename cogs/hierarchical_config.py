c0d393cf245f28eb7ed190971f893e23e8f51342
Michał Barczewski <michal.j.barczewski@gmail.com>
2016-09-11 20:53:22 +0200

    update functions to use Location

--- cogs/hierarchical_config.py
+++ cogs/hierarchical_config.py
@@ -18,25 +18,37 @@ class HierarchicalConfig:
     def ensure_path(self, path: List[str]) -> Dict[str, Dict]:
         if len(path) == 0:
             return self.data
-        current_location = self.data
+        current_location = self.data # type: Location
         for key in path:
-            if key not in current_location:
-                current_location[key] = dict()
-            current_location = current_location[key]
+            if key not in current_location.locations:
+                current_location.locations[key] = Location()
+            current_location = current_location.locations[key]
         return current_location
 
-    def save_var(self, name, value, path):
-        location = self.ensure_path(path)
-        location[name] = value
+    def get_location(self, path: List[str]):
+        location = self.data
+        for key in path:
+            if key in location.locations[key]:
+                location = location.locations[key]
+        return location
 
-    def get_var(self, name, path):
+    def save_var(self, path: List[str], name: str, value: ValueType):
+        location = self.ensure_path(path)
+        location.values[name] = value
+
+    def delete_var(self, path: List[str], name: str):
+        location = self.get_location(path)
+        if name in location.values[name]:
+            del location.values[name]
+
+    def get_var(self, path: List[str], name: str):
         value = self.data.get(name, default=self.defaults.get(name))
         location = self.data
         for key in path:
             # get value from current location, preserve existing one if it doesn't exist here
-            value = location.get(name, default=location)
-            if key in location:
-                location = location[key]
+            value = location.values.get(name, default=location)
+            if key in location.locations:
+                location = location.locations[key]
             else:
                 break
         return value
