ac171486454a99c738ebe7db0a591e52cb467db7
Michał Barczewski <michal.j.barczewski@gmail.com>
2016-09-12 16:07:53 +0200

    tests using Location class, some json stuff (that doesn't work)

--- cogs/hierarchical_config.py
+++ cogs/hierarchical_config.py
@@ -1,34 +1,47 @@
-from typing import Dict, List, TypeVar, Union
+import json
+from json.encoder import JSONEncoder
+from typing import Dict, List, Union, NewType
 
-BaseValueType = TypeVar('BaseValueType', Union[str, int, float])
-ValueType = TypeVar('ValueType', Union[BaseValueType, List[BaseValueType]])
+BaseValueType = NewType('BaseValueType', Union[str, int, float])
+ValueType = NewType('ValueType', Union[BaseValueType, List[BaseValueType]])
 
 
 class Location:
-    def __init__(self):
-        self.locations = dict()  # type: Dict[str, Location]
-        self.values = dict()  # type: Dict[str, ValueType]
+    def __init__(self, locations: Dict = None, values: Dict[str, ValueType] = None) -> object:
+        self.locations = locations if locations else {}  # type: Dict[str, Location]
+        self.values = values if values else {}  # type: Dict[str, ValueType]
+
+    def __eq__(self, other):
+        if not isinstance(other, Location):
+            return False
+        elif self.locations != other.locations:
+            return False
+        else:
+            return self.values == other.values
 
 
 class HierarchicalConfig:
     def __init__(self, defaults: Dict[str, ValueType] = None):
-        self.data = Location()
-        self.defaults = {} if defaults is None else defaults # type: Dict[str, ValueType]
+        self.data = Location() # type: Location
+        self.defaults = {} if defaults is None else defaults  # type: Dict[str, ValueType]
+
+    def dumps_json(self):
+        return json.dumps(self, cls=HierarchicalConfigJsonEncoder)
 
     def ensure_path(self, path: List[str]) -> Dict[str, Dict]:
         if len(path) == 0:
             return self.data
-        current_location = self.data # type: Location
+        location = self.data  # type: Location
         for key in path:
-            if key not in current_location.locations:
-                current_location.locations[key] = Location()
-            current_location = current_location.locations[key]
-        return current_location
+            if key not in location.locations:
+                location.locations[key] = Location()
+            location = location.locations[key]
+        return location
 
     def get_location(self, path: List[str]):
         location = self.data
         for key in path:
-            if key in location.locations[key]:
+            if key in location.locations:
                 location = location.locations[key]
         return location
 
@@ -42,14 +55,22 @@ class HierarchicalConfig:
             del location.values[name]
 
     def get_var(self, path: List[str], name: str):
-        value = self.data.get(name, default=self.defaults.get(name))
+        value = self.data.values.get(name, self.defaults.get(name))
         location = self.data
         for key in path:
             # get value from current location, preserve existing one if it doesn't exist here
-            value = location.values.get(name, default=location)
             if key in location.locations:
                 location = location.locations[key]
+                value = location.values.get(name, value)
             else:
                 break
         return value
+
+
+class HierarchicalConfigJsonEncoder(JSONEncoder):
+    def default(self, o):
+        if isinstance(o, HierarchicalConfig) or isinstance(o, Location):
+            json_str = json.dumps(o.__dict__, cls = HierarchicalConfigJsonEncoder)
+            return json_str
+        return JSONEncoder.default(self, o)
 
--- test/hierarchical_config_test.py
+++ test/hierarchical_config_test.py
@@ -1,53 +1,103 @@
+import json
 import unittest
 
-from cogs.hierarchical_config import HierarchicalConfig
+from cogs.hierarchical_config import HierarchicalConfig, Location
+
 
 class TestSettings(unittest.TestCase):
 
     def testEnsurePath(self):
         config = HierarchicalConfig()
 
-        self.assertDictEqual({}, config.data)
+        self.assertDictEqual({}, config.data.locations)
 
         path = []
         config.ensure_path(path)
-        self.assertDictEqual({}, config.data)
+        self.assertDictEqual({}, config.data.locations)
 
         path = ['a1']
         config.ensure_path(path)
-        self.assertDictEqual({'a1': {}}, config.data)
+        self.assertEqual({'a1': Location()}, config.data.locations)
 
         path = ['a1', 'b1']
         config.ensure_path(path)
-        self.assertDictEqual({
-            'a1': {
-                'b1': {}
-            }
-        }, config.data)
+        self.assertEqual({
+            'a1': Location({
+                'b1': Location()
+            })
+        }, config.data.locations)
 
         path = ['a2', 'b2']
         config.ensure_path(path)
         self.assertDictEqual({
-            'a1': {
-                'b1': {}
-            },
-            'a2': {
-                'b2': {}
-            }
-        }, config.data)
+            'a1': Location({
+                'b1': Location()
+            }),
+            'a2': Location({
+                'b2': Location()
+            })
+        }, config.data.locations)
 
         path = ['a1', 'b1', 'c1']
         config.ensure_path(path)
         self.assertDictEqual({
-            'a1': {
-                'b1': {
-                    'c1': {}
-                }
-            },
-            'a2': {
-                'b2': {}
-            }
-        }, config.data)
+            'a1': Location({
+                'b1': Location({
+                    'c1': Location()
+                })
+            }),
+            'a2': Location({
+                'b2': Location()
+            })
+        }, config.data.locations)
+
+    def testConfig(self):
+        config = HierarchicalConfig()
+
+        path = ['a1', 'b1', 'c1']
+        config.ensure_path(path)
+
+        location = config.get_location(['a1', 'b1'])
+        self.assertEqual(Location({'c1': Location()}), location)
+
+        value = 123
+        name = "abc"
+        config.save_var(path, name, value)
+
+        read_value = config.get_var(path, name)
+        self.assertEquals(123, read_value)
+
+        config.save_var([],"name", "v_root")
+        config.save_var(['lvl_1_a'], "name", "v_1a")
+
+        default_value = config.get_var(['lvl_1_b'], 'name')
+        self.assertEquals('v_root', default_value)
+
+        lvl_1_value = config.get_var(['lvl_1_a'], 'name')
+        self.assertEquals('v_1a', lvl_1_value)
+
+        lvl_2_value = config.get_var(['lvl_1_a', 'lvl_2_a'], 'name')
+        self.assertEquals('v_1a', lvl_2_value)
+
+        other_lvl_value = config.get_var(['a', 'b', 'c', 'd'], 'name')
+        self.assertEquals('v_root', other_lvl_value)
+
+        config.save_var([], 'other_name', 1)
+        other_name_value = config.get_var(['1'], 'other_name')
+        self.assertEquals(1, other_name_value)
+
+    def testJsonSerialize(self):
+        config = HierarchicalConfig()
+
+        config.save_var([], 'var1', 1)
+        config.save_var(['a1', 'b1'], 'var2', 'abc')
+        config.save_var(['a1'], 'var3', [1,2,3])
+
+        json_str = config.dumps_json()
+
+        loaded_config = json.loads(json_str)
+
+        self.assertEquals(config, loaded_config)
 
 if __name__ == '__main__':
     unittest.main()
