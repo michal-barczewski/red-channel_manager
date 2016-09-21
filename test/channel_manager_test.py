f64eabe2023d93bf8ef5db7a4e82360a34e88881
Michał Barczewski <michal.j.barczewski@gmail.com>
2016-09-21 22:25:38 +0200

    more tests

--- test/channel_manager_test.py
+++ test/channel_manager_test.py
@@ -1,6 +1,9 @@
+import argparse
 import unittest
 
 from cogs.channel_manager import find_free_numbers
+from cogs.hierarchical_config import HierarchicalConfig, Variable
+
 
 class TestUtils(unittest.TestCase):
 
@@ -15,7 +18,32 @@ class TestUtils(unittest.TestCase):
         self.assertEquals(find_free_numbers(free_numbers, 1), [1, 2, 5, 6, 7])
         self.assertEquals(find_free_numbers(free_numbers, 7), [1, 2, 5, 6, 7, 9, 10])
 
+    def test_int_variable(self):
+        config = HierarchicalConfig()
+        int_variable = Variable(
+            levels=['global', 'server', 'group'],
+            name='int_variable',
+            var_type='int',
+            default=1,
+            description='minimum time since last activity before channel is allowed to be deleted',
+            store=config
+        )
+        int_variable.set_global(5)
+        self.assertEquals(int_variable.get(), 5)
+        int_variable.set_global('5')
+        self.assertEquals(int_variable.get(), 5)
 
+        self.assertRaises(ValueError, int_variable.set_global, 'a')
 
+    def test_argparse(self):
+        parser = argparse.ArgumentParser(prog="set")
+        #parser.add_argument('variable_name', type=str, help='name of variable to set')
+        parser.add_argument('-level', type=str, default='global',help='specify on what level to set the value')
+        parser.add_argument('-channel_timeout', type=int, default=1,help='time since last channel activity before which channel can not be deleted')
+        print(
+            parser.parse_args(['-level', 'server',
+                               '-channel_timeout','1'])
+        )
+        #parser.print_help()
 if __name__ == '__main__':
     unittest.main()
